c9ac5811f64aa152efe23c638ebd5a457f52774b
ide user ide_captaineasycode <captaineasycode@gmail.com>
2012-06-06 02:39:11 +0000

    Complaining about changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('This should get checked into Git Hub');
+    res.end('It would have been nice if they said something about the ssh key first!');
 }).listen(process.env.PORT);
