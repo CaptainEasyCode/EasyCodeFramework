7228496e47d31ae78f328a1eedd7d33b0e1fc795
ide user ide_captaineasycode <captaineasycode@gmail.com>
2012-06-06 03:06:14 +0000

    People collection has a gender prop

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 var dbUrl = "BWhite:Tracy@staff.mongohq.com:10084/EasyDb";
-var collections = ["users", "reports"];
+var collections = ["People"];
 var db = require("mongo.js").connect(dbUrl, collections);
-db.users.find({sex: "female"}, function(err, users) {
+db.users.find({gender: "female"}, function(err, users) {
     if( err || !users) console.log("No femail users found");
     else users.forEach( function(femaleUser) {
         console.log(femaleUser);
