3d7a26387d4f2000e48d23578258cc3ef7dd971e
ide user ide_captaineasycode <captaineasycode@gmail.com>
2012-06-06 02:42:30 +0000

    Changing back to just hello world

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('It would have been nice if they said something about the ssh key first!');
+    res.end('Back to just Hello World');
 }).listen(process.env.PORT);
